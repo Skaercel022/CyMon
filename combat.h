50fc90320c72f4151478649339e5df43131d96bf
ChocoBn01 <nathaneviname@gmail.com>
2025-04-29 21:47:09 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -1,6 +1,7 @@
 #ifndef COMBAT_H
 #define COMBAT_H
 float type_effect(Type atk, Type def);
+void effect_status(Pokemon* poke, int* nbtour);
 void aff_effect_atk(Competence_spe atk);
 void aff_atk(Competence_spe atk);
 void speedbarplus(Pokemon* un, Pokemon* deux, Pokemon* trois, Pokemon* quatre, Pokemon* cinq, Pokemon* six);
@@ -8,7 +9,7 @@ int plus1000(Pokemon un, Pokemon deux, Pokemon trois, Pokemon quatre, Pokemon ci
 Comp choix_atk(Pokemon* bu);
 int attaque_esquivee(Comp atk, Pokemon* attaque, Pokemon* defend);
 float degats(Pokemon* attaquant, Pokemon* defenseur, Comp atq);
-void effet_spe(Pokemon* attaquant, Pokemon* defenseur, Comp atk);
+void effet_spe(Pokemon** attaquant, Pokemon* defenseur, Comp atk);
 Comp maj_damage(Pokemon* offense, Pokemon** adversaires, Pokemon** cible);
 Comp choix_bot_spe(Pokemon* offense, Pokemon** adversaires, Pokemon** cible, int nb);
 void fin_e_a_b(Pokemon* offense, Pokemon* cible, Comp choix);
@@ -19,6 +20,6 @@ void effect_atk(Pokemon* offense, Pokemon** team, Pokemon** adversaires, Comp at
 Pokemon* maj(Pokemon* un, Pokemon* deux);
 Pokemon* maj6(Pokemon* un, Pokemon* deux, Pokemon* trois, Pokemon* quatre, Pokemon* cinq, Pokemon* six);
 void cooldown1poke(Pokemon* poke);
-void effect_status(Pokemon* poke, int nbtour);
+void aff_states(Pokemon* poke);
 int fight(Pokemon** player, Pokemon** bot, int mode);
 #endif 
