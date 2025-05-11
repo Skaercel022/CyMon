e20bf7327170f593bceb4c019caaae79ac91defe
ChocoBn01 <nathaneviname@gmail.com>
2025-05-11 13:47:38 +0200

    Update creapers.h

--- creapers.h
+++ creapers.h
@@ -2,8 +2,8 @@
 #define CREAPERS_H
 void crea_comp(Comp* comp, Competence_spe comp_spe); 
 void create_part2_poke(FILE* file, Pokemon* poke, int* nb);
-Pokemon* create_part1_poke_joueur(int* numero);
+Pokemon* create_part1_poke_joueur(int* numero, int unoudeux);
 Pokemon* create_part1_poke_ordi(int* numero);
-Pokemon** create_team_joueur(int* numero);
+Pokemon** create_team_joueur(int* numero, int unoudeux);
 Pokemon** create_team_ordi(int* numero);
 #endif
