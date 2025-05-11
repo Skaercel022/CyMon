a49800a9157c1294e7a4d85d25e68f55101dd4ce
ChocoBn01 <nathaneviname@gmail.com>
2025-05-11 16:52:55 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -1,26 +1,24 @@
 #ifndef COMBAT_H
 #define COMBAT_H
-void vide_buffer();
 float type_effect(Type atk, Type def);
-void effect_status(Pokemon* poke, int* nbtour);
+void effect_status(Pokemon* poke, int* nbtour, SDL_Renderer* rend);
 void aff_effect_atk(Competence_spe atk);
 void aff_atk(Competence_spe atk);
 void speedbarplus(Pokemon* un, Pokemon* deux, Pokemon* trois, Pokemon* quatre, Pokemon* cinq, Pokemon* six);
 int plus1000(Pokemon un, Pokemon deux, Pokemon trois, Pokemon quatre, Pokemon cinq, Pokemon six);
-Comp choix_atk(Pokemon* bu);
+Comp choix_atk(Pokemon* bu, SDL_Renderer* rend);
 int attaque_esquivee(Comp atk, Pokemon* attaque, Pokemon* defend);
 float degats(Pokemon* attaquant, Pokemon* defenseur, Comp atq);
-void effet_spe(Pokemon** attaquant, Pokemon* defenseur, Comp atk);
+void effet_spe(Pokemon** attaquant, Pokemon* defenseur, Comp atk, SDL_Renderer* rend);
 Comp maj_damage(Pokemon* offense, Pokemon** adversaires, Pokemon** cible);
 Comp choix_bot_spe(Pokemon* offense, Pokemon** adversaires, Pokemon** cible, int nb);
-void fin_e_a_b(Pokemon* offense, Pokemon* cible, Comp choix);
-void effect_atk_bot(Pokemon* offense, Pokemon** team, Pokemon** adversaires);
-Pokemon* choix_target(Pokemon** team, Pokemon** opp);
-void fin_e_a(Pokemon* offense, Pokemon* cible, Comp atk);
-void effect_atk(Pokemon* offense, Pokemon** team, Pokemon** adversaires, Comp atk);
+void fin_e_a_b(Pokemon* offense, Pokemon* cible, Comp choix, SDL_Renderer* rend);
+void effect_atk_bot(Pokemon* offense, Pokemon** team, Pokemon** adversaires, SDL_Renderer* rend);
+Pokemon* choix_target(Pokemon** team, Pokemon** opp, SDL_Renderer* rend);
+void fin_e_a(Pokemon* offense, Pokemon* cible, Comp atk, SDL_Renderer* rend);
+void effect_atk(Pokemon* offense, Pokemon** team, Pokemon** adversaires, Comp atk, SDL_Renderer* rend);
 Pokemon* maj(Pokemon* un, Pokemon* deux);
 Pokemon* maj6(Pokemon* un, Pokemon* deux, Pokemon* trois, Pokemon* quatre, Pokemon* cinq, Pokemon* six);
 void cooldown1poke(Pokemon* poke);
-void aff_states(Pokemon* poke);
-int fight(Pokemon** player, Pokemon** bot, int mode);
+int fight(Pokemon** player, Pokemon** bot, int mode, SDL_Renderer* rend, SDL_Window* wind);
 #endif 
